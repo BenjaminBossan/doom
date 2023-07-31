aa30c3c11d1bfe498921ef4904c581380d57566a
Benjamin Bossan <benjamin.bossan@gmail.com>
2023-07-31 11:44:56 +0200

    Add ruff support for flycheck

--- config.el
+++ config.el
@@ -408,6 +408,33 @@
 (add-hook 'shell-mode-hook 'my-python-pdb-shell-mode-hook)
 
 
+;; add ruff support to flycheck
+;; see https://github.com/flycheck/flycheck/issues/1974#issuecomment-1343495202
+(flycheck-define-checker python-ruff
+  "A Python syntax and style checker using the ruff utility.
+To override the path to the ruff executable, set
+`flycheck-python-ruff-executable'.
+See URL `http://pypi.python.org/pypi/ruff'."
+  :command ("ruff"
+            "--format=text"
+            (eval (when buffer-file-name
+                    (concat "--stdin-filename=" buffer-file-name)))
+            "-")
+  :standard-input t
+  :error-filter (lambda (errors)
+                  (let ((errors (flycheck-sanitize-errors errors)))
+                    (seq-map #'flycheck-flake8-fix-error-level errors)))
+  :error-patterns
+  ((warning line-start
+            (file-name) ":" line ":" (optional column ":") " "
+            (id (one-or-more (any alpha)) (one-or-more digit)) " "
+            (message (one-or-more not-newline))
+            line-end))
+  :modes python-mode)
+
+(add-to-list 'flycheck-checkers 'python-ruff)
+
+
 ;; copilot.el: https://github.com/zerolfx/copilot.el#example-for-doom-emacs
 (use-package! copilot
   ;; uncomment to activate copilot automatically
